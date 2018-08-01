c8899edf1236fdeeb1ca0ff4b56c4b3782a84171
Pavel Palancica <pavel.palancica@gmail.com>
2018-08-01 23:20:43 +0300

    First commit. Created the Single View Application project in Xcode.

---
+++ TableViewDemo/TableViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewDemo
+//
+//  Created by Pavel Palancica on 8/1/18.
+//  Copyright © 2018 Pavel Palancica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
